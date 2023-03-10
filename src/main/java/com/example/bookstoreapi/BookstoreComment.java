5122025fed0ab96052444076a7ca72456eef505a
MeanMontague <62820925+MeanMontague@users.noreply.github.com>
2023-03-09 23:14:56 -0500

    Delete src directory

--- src/main/java/com/example/bookstoreapi/BookstoreComment.java
+++
@@ -1,6 +0,0 @@
-package com.example.bookstoreapi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface BookstoreComment extends JpaRepository<Comment, Integer> {
-}
-
--- src/main/java/com/example/bookstoreapi/BookstoreWishlistRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookstoreapi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface BookstoreWishlistRepository extends JpaRepository<Wishlist, Integer> {
-}
