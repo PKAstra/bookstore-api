3d0a28150960c5125f03d25399112cc036ceb20f
William Ricardo <Ricardo.william97@gmail.com>
2023-03-17 22:23:07 -0400

    Retrieve List of Top Sellers Update

--- src/main/java/com/example/bookstoreapi/BookstoreRepository.java
+++ src/main/java/com/example/bookstoreapi/BookstoreRepository.java
@@ -11,4 +11,6 @@ public interface BookstoreRepository extends JpaRepository<Book, Integer> {
     @Query(value = "SELECT b FROM Book b WHERE b.genre = ?1")
     public List<Book> findBookByGenre(String genre);
 
+    @Query(value = "SELECT * FROM books ORDER BY copies_sold DESC LIMIT 10", nativeQuery = true)
+    public List<Book> findTop10BooksByCopiesSold();
 }
