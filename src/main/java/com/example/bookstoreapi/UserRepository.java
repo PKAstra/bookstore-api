924622dd75a29778654fcf0da8e9637703dc522b
MeanMontague <62820925+MeanMontague@users.noreply.github.com>
2023-03-24 23:36:50 -0400

    Update UserRepository.java
    update queries

--- src/main/java/com/example/bookstoreapi/UserRepository.java
+++ src/main/java/com/example/bookstoreapi/UserRepository.java
@@ -1,6 +1,16 @@
 package com.example.bookstoreapi;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, String> {
-    User findByUsername(String username);
+    @Query(value = "SELECT b FROM User b WHERE b.username = ?1")
+    public List<User> getUserByUsername(String username);
+    @Transactional @Modifying(clearAutomatically = true)
+    @Query(value = "INSERT INTO Users (username, password, name, email, home_address) VALUES (?1, ?2, ?3, ?4, ?5)", nativeQuery = true)
+    void createUser(String username, String password, String name, String email, String home_address);
 }
