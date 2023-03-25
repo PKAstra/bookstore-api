c5d784e5d33b54b4987ad2a9507eca9486334dd9
MeanMontague <62820925+MeanMontague@users.noreply.github.com>
2023-03-25 00:02:42 -0400

    Changes to getUserByUsername

--- src/main/java/com/example/bookstoreapi/BookstoreService.java
+++ src/main/java/com/example/bookstoreapi/BookstoreService.java
@@ -107,9 +107,17 @@ public class BookstoreService {
         }
     }
 
-       public List<User> getUserByUsername(String username){
+       public ResponseEntity<?> getUserByUsername(String username){
        //  logic
-        return userRepository.findAll();
+           logger.info("Matching Users:");
+           List<User> users = userRepository.getUserByUsername(username);
+
+
+           return new ResponseEntity<>(users,HttpStatus.OK);
+       }
+    public ResponseEntity createUser(String username, String password, String name, String email, String home_address){
+        userRepository.createUser(username, password, name, email, home_address);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
 
