85ea591e86f2d983f920b14f386fd6f09175d2a4
Andres Rodriguez Toca <48394901+AndresRodriguezToca@users.noreply.github.com>
2023-03-26 19:00:13 -0400

    Update BookstoreRepository.java

--- src/main/java/com/example/bookstoreapi/BookstoreRepository.java
+++ src/main/java/com/example/bookstoreapi/BookstoreRepository.java
@@ -16,6 +16,9 @@ public interface BookstoreRepository extends JpaRepository<Book, Integer> {
     @Query(value = "SELECT * FROM books ORDER BY copies_sold DESC LIMIT 10", nativeQuery = true)
     public List<Book> findTop10BooksByCopiesSold();
 
+    @Query(value = "SELECT b FROM Book b WHERE b.rating >= ?1")
+    public List<Book> findBooksByRating(Double rating);
+
     @Transactional
     @Modifying(clearAutomatically = true)
     @Query(value = "INSERT INTO books (isbn, title, description, author, genre, price, copies_sold, publisher, year_published, book_id) VALUES (?1, ?2, ?3, ?5, ?6, ?4, ?9, ?7, ?8, 6)", nativeQuery = true)
