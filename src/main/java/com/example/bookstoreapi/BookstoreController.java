4fe2c29bf4c6429fcd8372b3ab70db382ba28058
Jonathan Rivero <58375544+jonriv48376@users.noreply.github.com>
2023-04-09 23:55:41 -0400

    Average rating feature

--- src/main/java/com/example/bookstoreapi/BookstoreController.java
+++ src/main/java/com/example/bookstoreapi/BookstoreController.java
@@ -1,13 +1,22 @@
 
 package com.example.bookstoreapi;
 
+import java.util.List;
+import java.util.ArrayList;
+import com.example.bookstoreapi.Order.Order;
+import com.example.bookstoreapi.Order.OrderDTO;
+import com.example.bookstoreapi.Order.ResponseOrderDTO;
+import com.example.bookstoreapi.ShoppingCart.ResponseShoppingCartDTO;
+import com.example.bookstoreapi.ShoppingCart.ShoppingCart;
+import com.example.bookstoreapi.ShoppingCart.ShoppingCartDTO;
+import com.example.bookstoreapi.ShoppingCart.ShoppingCartItems;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/bookstore")
@@ -37,11 +46,200 @@ public class BookstoreController {
         return new ResponseEntity(this.bookstoreService.getAllBooksFromWishlist(), HttpStatus.ACCEPTED);
     }
 
+    @DeleteMapping("/removeBookWishlist")
+    public ResponseEntity deleteBookWishlist(
+            @RequestParam(value = "wishlist_id") Integer wishlist_id,
+            @RequestParam(value = "book_id") Integer book_id) {
+        return this.bookstoreService.deleteBookWishlist(wishlist_id, book_id);
+    }
+
+    @PostMapping("/addBookWishlist")
+    public ResponseEntity addBookWishlist(
+            @RequestParam(value = "wishlist_id") Integer wishlist_id,
+            @RequestParam(value = "book_id") Integer book_id) {
+        return this.bookstoreService.addBookWishlist(wishlist_id, book_id);
+    }
+
+    @PostMapping("/addWishlist")
+    public ResponseEntity addWishlist(
+            @RequestParam(value = "wishlist_name") String wishlist_name,
+            @RequestParam(value = "user_id") Integer user_id) {
+        return this.bookstoreService.addWishlist(wishlist_name, user_id);
+    }
+
     @GetMapping("/getAllBookComments")
-    public ResponseEntity getAllBookComments(){
+    public ResponseEntity getAllBookComments() {
         return new ResponseEntity(this.bookstoreService.getAllBookComments(), HttpStatus.ACCEPTED);
     }
 
+    @GetMapping("/getBooksByGenre/{genre}")
+    public ResponseEntity getBooksByGenre(@PathVariable("genre") String genre) {
+        return this.bookstoreService.findBooksByGenre(genre);
+    }
+
+    @GetMapping("/getTop10BooksSold")
+    public ResponseEntity getTop10BooksSold() {
+        return this.bookstoreService.findTop10BooksSold();
+    }
+
+    @GetMapping("/getBooksByRating/{rating}")
+    public ResponseEntity getBooksByRating(@PathVariable("rating") Double rating) {
+        return this.bookstoreService.findBooksByRating(rating);
+    }
+
+    @GetMapping("/getUserByUsername/{username}")
+    public ResponseEntity getUserByUsername(@PathVariable("username") String username) {
+        return new ResponseEntity(this.bookstoreService.getUserByUsername(username), HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/createUser")
+    public ResponseEntity createUser(
+            @RequestParam(value = "username") String username,
+            @RequestParam(value = "password") String password,
+            @RequestParam(value = "name") String name,
+            @RequestParam(value = "email") String email,
+            @RequestParam(value = "home_address") String home_address) {
+        return this.bookstoreService.createUser(username, password, name, email, home_address);
+    }
+    //    KENNETH ENDPOINT CONTROLLER
+    @PostMapping("/addBook")
+    public ResponseEntity addBook(
+            @RequestParam(value = "ISBN") String ISBN,
+            @RequestParam(value = "title") String title,
+            @RequestParam(value = "description") String description,
+            @RequestParam(value = "price") Double price,
+            @RequestParam(value = "author") String author,
+            @RequestParam(value = "genre") String genre,
+            @RequestParam(value = "publisher") String publisher,
+            @RequestParam(value = "year") Integer year,
+            @RequestParam(value = "copies_sold") Integer copies_sold) {
+        return new ResponseEntity(this.bookstoreService.addNewBook(ISBN, title, description, price, author, genre, publisher, year, copies_sold), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{publisherName}/discount/{discountPercent}")
+    public ResponseEntity<String> updateDiscountPercentByPublisherName(
+            @PathVariable String publisherName,
+            @PathVariable Double discountPercent) {
+
+        bookstoreService.updateBooksDiscountByPublisher(publisherName, discountPercent);
+        return ResponseEntity.ok("Discount percent for publisher '" + publisherName + "' updated to " + discountPercent);
+    }
+
+    @GetMapping("/getBooksByPublisher/{publisherName}")
+    public ResponseEntity getBooksByPublisher(@PathVariable("publisherName") String publisher){
+        return new ResponseEntity(this.bookstoreService.findBooksByPublisher(publisher), HttpStatus.OK);
+    }
+
+    //SHOPPING CART CONTROLLER
+
+    //Retrieves User's Shopping Cart
+    @GetMapping("/shopping-cart/user-{userid}")
+    public ResponseEntity<ShoppingCart> getShoppingCart(@PathVariable int userid)
+    {
+        ShoppingCart shoppingCart = this.bookstoreService.getShoppingCart(userid);
+        return ResponseEntity.ok(shoppingCart);
+    }
+
+    //Retrieves User's Shopping Cart Subtotal
+    @GetMapping( "/shopping-cart/user-{userid}/subtotal")
+    public ResponseEntity<String> getShoppingCartSubtotal(@PathVariable int userid)
+    {
+        ShoppingCart shoppingCart = this.bookstoreService.getShoppingCart(userid);
+        double subtotal = this.bookstoreService.getShoppingCartSubtotal(shoppingCart.getItems());
+
+        String cartSubtotal = "Shopping Cart Subtotal: $" + subtotal;
+        return ResponseEntity.ok(cartSubtotal);
+    }
+    //Adds Book to Shopping Cart
+    @PostMapping("/shopping-cart/addbook")
+    public ResponseEntity<ResponseShoppingCartDTO> addBook(@RequestBody ShoppingCartDTO cart)
+    {
+        int customerID = cart.getCustomerID();
+        //Shopping Cart Item List
+        List<ShoppingCartItems> shoppingCartItems = new ArrayList<>();
+
+
+        //If Customer Does NOT have an Existing Shopping Cart
+        if (this.bookstoreService.getShoppingCart(customerID) == null)
+        {
+            //Adds Book to Shopping Cart Based Off bookID
+            shoppingCartItems.add(this.bookstoreService.addBookForNewCustomer(cart.getItems()));
+        }
+        //If Customer Already has an Existing Shopping Cart
+        else
+        {
+            ShoppingCart shoppingCart = this.bookstoreService.getShoppingCart(customerID);
+            //Adds New Book to Shopping Cart List Along With Existing Items
+            shoppingCartItems = this.bookstoreService.addBookForExistingCustomer(shoppingCart, cart.getItems());
+        }
+
+
+        ShoppingCartDTO shoppingCartDTO = new ShoppingCartDTO(customerID, shoppingCartItems);
+
+        //Saves Updated Shopping Cart
+        ShoppingCart shoppingCart = new ShoppingCart(customerID, shoppingCartItems);
+        this.bookstoreService.saveShoppingCart(shoppingCart);
+
+
+        //Response
+        ResponseShoppingCartDTO responseShoppingCartDTO = new ResponseShoppingCartDTO();
+        responseShoppingCartDTO.setCustomerID(shoppingCartDTO.getCustomerID());
+
+
+        return ResponseEntity.ok(responseShoppingCartDTO);
+    }
+
+    //Deletes Book from Shopping Cart
+    @DeleteMapping("/shopping-cart/deletebook/user-{userid}/book-{bookid}")
+    public ResponseEntity<String> deleteBook(@PathVariable int userid, @PathVariable int bookid)
+    {
+        String status = "Book Deleted...";
+
+        //Retrieves User's Shopping Cart
+        ShoppingCart shoppingCart =  this.bookstoreService.getShoppingCart(userid);
+        List<ShoppingCartItems> shoppingCartItems = new ArrayList<>();
+        //Deletes Requested Book
+        shoppingCartItems = this.bookstoreService.deleteBook(shoppingCart.getItems(), bookid);
+        //Saves New Shopping Cart
+        shoppingCart.setItems(shoppingCartItems);
+        this.bookstoreService.saveShoppingCart(shoppingCart);
+
+        return ResponseEntity.ok(status);
+    }
+
+
+//ORDER CONTROLLER
+
+    //Retrieves Order by Order ID
+    @GetMapping("/getOrder/{orderID}")
+    public ResponseEntity<Order> getOrderDetails(@PathVariable int orderID)
+    {
+        Order order = this.bookstoreService.getOrderDetail(orderID);
+        return ResponseEntity.ok(order);
+    }
+
+    //Retrieves Order by Order ID
+    @GetMapping("/getOrder/customer-{userID}")
+    public ResponseEntity<List<Order>> getCustomerOrders(@PathVariable int userID)
+    {
+        List<Order> customerOrders = this.bookstoreService.getCustomerOrders(userID);
+        return ResponseEntity.ok(customerOrders);
+    }
+
+
+    //Place Order by Customer ID
+    @PostMapping("/placeOrder")
+    public ResponseEntity<ResponseOrderDTO> placeOrder(@RequestBody OrderDTO order)
+    {
+        //Creates and Saves a New Order
+        Order newOrder = this.bookstoreService.placeOrder(order.getCustomerID());
+        this.bookstoreService.saveOrder(newOrder);
+        //Response
+        ResponseOrderDTO responseOrderDTO = new ResponseOrderDTO(newOrder.getId(), newOrder.getCustomerID(), newOrder.getOrderTotal());
+
+        return ResponseEntity.ok(responseOrderDTO);
+    }
+    //Rating and Commenting
     @GetMapping("/getAllBookRatings")
     public ResponseEntity getAllBookRatings(){
         return new ResponseEntity(this.bookstoreService.getAllBookRatings(), HttpStatus.ACCEPTED);
--- src/main/java/com/example/bookstoreapi/BookstoreService.java
+++ src/main/java/com/example/bookstoreapi/BookstoreService.java
@@ -1,27 +1,44 @@
 
 package com.example.bookstoreapi;
 
+import com.example.bookstoreapi.Order.Order;
+import com.example.bookstoreapi.Order.OrderItems;
+import com.example.bookstoreapi.Order.OrderRepo;
+import com.example.bookstoreapi.ShoppingCart.ShoppingCart;
+import com.example.bookstoreapi.ShoppingCart.ShoppingCartItems;
+import com.example.bookstoreapi.ShoppingCart.ShoppingCartRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.sql.Timestamp;
 
 
 @Service
 public class BookstoreService {
-    
+    private static final Logger logger = LoggerFactory.getLogger(BookstoreService.class);
+
     @Autowired
     BookstoreRepository bookstoreRepo;
     @Autowired
-    BookstoreWishlist bookstoreWish;
+    BookstoreWishlistRepository bookstoreWish;
     @Autowired
     BookstoreWishlistBooks bookstoreWishlistBooks;
     @Autowired
     BookstoreComment bookstoreComment;
     @Autowired
-    BookstoreRating bookstoreRating;
+    UserRepository userRepository;
+    @Autowired
+    ShoppingCartRepo shoppingCartRepo;
+    @Autowired
+    OrderRepo orderRepo;
 
     public List<Book> getAllBooks(){
         // logic
@@ -38,11 +55,275 @@ public class BookstoreService {
         return bookstoreWishlistBooks.findAll();
     }
 
+    public ResponseEntity deleteBookWishlist(Integer wishlist_id, Integer book_id){
+        WishlistBooks wishlistBooks = bookstoreWishlistBooks.findBookWishlist(wishlist_id, book_id);
+
+        if(null != wishlistBooks){
+            bookstoreWishlistBooks.delete(wishlistBooks);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity addBookWishlist(Integer wishlist_id, Integer book_id){
+        bookstoreWishlistBooks.addBookWishlistQuery(wishlist_id, book_id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public ResponseEntity addWishlist(String wishlist_name, Integer user_id){
+        bookstoreWish.addWishlistQuery(wishlist_name, user_id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
     public List<Comment> getAllBookComments(){
         // logic
         return bookstoreComment.findAll();
     }
 
+    public ResponseEntity<?> findBooksByGenre(String genre){
+        logger.info("Books with genre to find: " + genre);
+        List<Book> books = bookstoreRepo.findBookByGenre(genre);
+
+        if(!books.isEmpty()){
+            logger.info("Books found in db");
+            return new ResponseEntity<>(books, HttpStatus.OK);
+        }else{
+            logger.error("Books not found with genre: " + genre);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+    }
+
+    public ResponseEntity<?> findTop10BooksSold(){
+        logger.info("Top 10 best selling books: ");
+        List<Book> books = bookstoreRepo.findTop10BooksByCopiesSold();
+        if(!books.isEmpty()){
+            logger.info("Books found in db.");
+            return new ResponseEntity<>(books, HttpStatus.OK);
+        }else{
+            logger.error("Books not found.");
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    public ResponseEntity<?> findBooksByRating(Double rating) {
+        logger.info("Books with rating " + rating + " or higher:");
+        List<Book> books = bookstoreRepo.findBooksByRating(rating);
+
+        if (!books.isEmpty()) {
+            logger.info("Books found in db");
+            return new ResponseEntity<>(books, HttpStatus.OK);
+        } else {
+            logger.error("No books found with rating " + rating + " or higher");
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    public ResponseEntity<?> getUserByUsername(String username){
+        //  logic
+        logger.info("Matching Users:");
+        List<User> users = userRepository.getUserByUsername(username);
+
+
+        return new ResponseEntity<>(users,HttpStatus.OK);
+    }
+    public ResponseEntity createUser(String username, String password, String name, String email, String home_address){
+        userRepository.createUser(username, password, name, email, home_address);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    //    KENNETH ENDPOINT SERVICE
+    public ResponseEntity addNewBook(String ISBN, String title, String description, Double price, String author, String genre, String publisher, Integer year, Integer copies_sold){
+        bookstoreRepo.addNewBook(ISBN, title, description, price, author, genre, publisher, year, copies_sold);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public List<Book> findBooksByPublisher(String publisher) {
+        return bookstoreRepo.findByPublisherName(publisher);
+    }
+
+    public ResponseEntity updateBooksDiscountByPublisher(String publisherName, Double discountPercent) {
+        List<Book> books = bookstoreRepo.findByPublisherName(publisherName);
+
+        for (Book book : books) {
+            book.setDiscountPercent(discountPercent);
+        }
+
+        bookstoreRepo.saveAll(books);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+//SHOPPING CART SERVICE
+
+    //Retrieves User's Shopping Cart
+    public ShoppingCart getShoppingCart(int userID) {
+        Optional<ShoppingCart> shoppingCart = this.shoppingCartRepo.findById(userID);
+        return shoppingCart.isPresent() ? shoppingCart.get() : null;
+    }
+
+    //Adds New Book to Existing Customer's Shopping Cart
+    public List<ShoppingCartItems> addBookForExistingCustomer(ShoppingCart shoppingCart, List<ShoppingCartItems> newBook)
+    {
+
+        //Creates a New Shopping Cart List
+        List<ShoppingCartItems> shoppingCartItems;
+        //Fills New Shopping Cart List with Previous Items in Cart
+        shoppingCartItems = shoppingCart.getItems();
+        //Adds New Item to New Shopping Cart List
+        for(ShoppingCartItems tempBook : newBook)
+        {
+            //Creates Book Object Based Off bookID
+            int bookID = tempBook.getBookID();
+            Optional<Book> book = bookstoreRepo.findById(bookID);
+            ShoppingCartItems shoppingCartItem = new ShoppingCartItems(book);
+            shoppingCartItems.add(shoppingCartItem);
+        }
+
+        //Returns New Shopping Cart List
+        return shoppingCartItems;
+
+    }
+
+    //Calculates Shopping Cart Subtotal
+    public Double getShoppingCartSubtotal(List<ShoppingCartItems> shoppingCartItems)
+    {
+        Double shoppingCartSubtotal = 0.0;
+
+        //Loops Through Entire List of User "Added" Books (Based off User Input in Postman)
+        for(ShoppingCartItems shoppingCartItem : shoppingCartItems)
+        {
+            int bookID = shoppingCartItem.getBookID();
+            Optional<Book> book = bookstoreRepo.findById(bookID);
+            //Checks to See if Book ID Entered by User Exist in Database
+            if(book.isPresent())
+            {
+                Book book1 = book.get();
+                //Adds Price of Book in Question to Shopping Cart Subtotal
+                shoppingCartSubtotal = + shoppingCartSubtotal + book1.getPrice();
+                //Adds Book Name, ID, and Price to Shopping Cart
+                shoppingCartItem.setBookName(book1.getTitle());
+                shoppingCartItem.setBookPrice(book1.getPrice());
+            }
+        }
+
+        return shoppingCartSubtotal;
+    }
+    //Finds Book, Adds to Shopping Cart, and Returns Cart
+    public ShoppingCartItems addBookForNewCustomer(List<ShoppingCartItems> cart)
+    {
+        ShoppingCartItems shoppingCartItems = new ShoppingCartItems();
+        for(ShoppingCartItems tempCart : cart)
+        {
+            int bookID = tempCart.getBookID();
+            Optional<Book> book = bookstoreRepo.findById(bookID);
+            shoppingCartItems = new ShoppingCartItems(book);
+        }
+        return shoppingCartItems;
+    }
+
+    public List<ShoppingCartItems> deleteBook(List<ShoppingCartItems> cart, int bookid)
+    {
+        //Creates a New Shopping Cart List
+        List<ShoppingCartItems> shoppingCartItems = new ArrayList<>();
+
+        //Copies Existing Shopping Cart MINUS Deleted Book to New Shopping Cart List
+        for(ShoppingCartItems tempBook : cart)
+        {
+            if(tempBook.getBookID() != bookid)
+            {
+                shoppingCartItems.add(tempBook);
+            }
+        }
+
+        //Returns New Shopping Cart List
+        return shoppingCartItems;
+    }
+
+    //Saves Shopping Cart
+    public ShoppingCart saveShoppingCart(ShoppingCart shoppingCart)
+    {
+        return shoppingCartRepo.save(shoppingCart);
+    }
+
+
+
+//ORDER SERVICE
+
+    //Retrieves Order by Order ID
+    public Order getOrderDetail(int orderId)
+    {
+        Optional<Order> order = this.orderRepo.findById(orderId);
+        return order.isPresent() ? order.get() : null;
+    }
+
+    //Retrieves Customer Orders
+    public List<Order> getCustomerOrders(int customerID)
+    {
+        //Retrieves All Orders
+        List<Order> tempOrders = this.orderRepo.findAll();
+        //Stores Customer Specific Orders
+        List<Order> customerOrders = new ArrayList<>();
+        //Loops Through All Orders and Extracts Customer Specific Orders
+        for(Order temp:tempOrders)
+        {
+            if(temp.getCustomerID() == customerID)
+            {
+                customerOrders.add(temp);
+            }
+        }
+        //Returns Null if No Orders found
+        if(customerOrders.isEmpty())
+        {
+            return null;
+        }
+
+        return customerOrders;
+    }
+
+    public Order placeOrder(int customerID)
+    {
+        //Retrieves Customer's Shopping Cart
+        Optional<ShoppingCart> shoppingCart = this.shoppingCartRepo.findById(customerID);
+
+
+        //Subtotal
+        double subtotal = getShoppingCartSubtotal(shoppingCart.get().getItems());
+        //Order Number
+        int orderNumber = new Random().nextInt(100);
+        while(getOrderDetail(orderNumber) != null)
+        {
+            //Ensures Order Number is NOT in Use
+            orderNumber = new Random().nextInt(100);
+        }
+        //Items in Order
+        List<OrderItems> orderItems = new ArrayList<>();
+
+
+        //Loops Through User's Shopping Cart, Adding Each Item to the Order
+        for(ShoppingCartItems items : shoppingCart.get().getItems())
+        {
+            int bookID = items.getBookID();
+            OrderItems tempOrderItems = new OrderItems(this.bookstoreRepo.findById(bookID));
+            orderItems.add(tempOrderItems);
+        }
+
+        //Clears Customer's Shopping Cart
+        this.shoppingCartRepo.findById(customerID).get().setItems(null);
+
+        //Creates a New Order With All the Items in the User's Cart
+        Order newOrder = new Order(orderNumber,customerID,orderItems, subtotal);
+
+
+        return newOrder;
+    }
+
+    //Saves Order
+    public Order saveOrder(Order newOrder)
+    {
+        return orderRepo.save(newOrder);
+    }
+//Ratings and Comments
     public List<Rating> getAllBookRatings(){
         return  bookstoreRating.findAll();
     }
